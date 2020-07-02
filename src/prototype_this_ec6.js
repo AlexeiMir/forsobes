fec1d028d3aa83309b64c039350864394a4e67b9
AlexeiMir <alex_16.07@mail.ru>
2020-07-02 16:56:42 +0700

    prototype this ec6

--- src/prototype_this_ec6.js
+++ src/prototype_this_ec6.js
@@ -27,3 +27,31 @@ class Cat extends Animal {
 }
 
 const cat = new Cat({name: 'Murzik', color: '#000', hasTail: true})
+
+
+Object.prototype.print = function () {
+    console.log("This object is", this)
+}
+
+cat.print()
+
+Array.prototype.mapAndLog = function () {
+    console.log("Array to map", this)
+    return this.map.apply(this,arguments) // map является функцией и параметры туда передвем с помощью apply
+}
+
+console.log([1,2,3,4].mapAndLog(x => x**2))
+
+String.prototype.toTag = function(tagName) {
+    return `<${tagName}>${this}</${tagName}>`
+}
+
+console.log('eminem'.toTag('strong'))
+console.log('eminem'.toTag('em'))
+
+Number.prototype.toBigInt = function () {
+return BigInt(this)
+}
+
+const number = 47
+console.log(number.toBigInt())
